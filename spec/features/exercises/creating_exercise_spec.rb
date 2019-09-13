d8eb5d0fd75e994baafb0efb4f0b5632198bb399
Anush Reddy <anushreddy04@gmail.com>
2019-09-13 18:26:18 -0500

    Implement create, show actions and show view template

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -14,8 +14,8 @@ RSpec.feature "Creating Exercise" do
     expect(page).to have_link "Back"
 
     fill_in "Duration", with: 70
-    fill_in "Workout Details", with: "Weight Lifting"
-    fill_in "Acivity Date", with: "2019-07-23"
+    fill_in "Workout details", with: "Weight Lifting"
+    fill_in "Activity date", with: "2019-07-23"
     click_button "Create Exercise"
 
     expect(page).to have_content("Exercise has been created")
